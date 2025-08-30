7bc89e3a93f9f248d87a25414bb09972f72ba4e0
Florian Teich <florianteich@gmail.com>
2025-08-30 20:41:55 +0200

    fix prettier settings and makefile

--- {{project_name}}/main.py
+++ {{project_name}}/main.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
-"""
-Main script demonstrating the use of lib_a package
-"""
+"""Main script demonstrating the use of lib_a package."""
+
+import logging
 
 from lib_a import hello_world
 
+logger = logging.getLogger(__name__)
+
+
 def main():
-    """Main function"""
+    """Main function."""
     message = hello_world()
-    print(message)
-    print("Hello from completely-new!")
+    logger.info(message)
+    logger.info("Hello from completely-new!")
+
 
 if __name__ == "__main__":
     main()
